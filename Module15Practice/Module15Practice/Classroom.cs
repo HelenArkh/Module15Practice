e4b47520b98d39d6558166b1fa863c988e9d2ffb
HelenArkh <alena.arkh@gmail.com>
2021-03-25 18:55:41 +0300

    Решение практической задачи

---
+++ Module15Practice/Module15Practice/Classroom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module15Practice
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Module15Practice/Module15Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Module15Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+          {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.Select(c => c.Students).SelectMany(s => s).ToArray();
+        }
+    }
+}
